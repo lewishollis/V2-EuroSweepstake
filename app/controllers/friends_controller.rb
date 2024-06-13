d968f549ad7d588d2f25a83dba6acbc985a93fc6
Lewis Hollis <111375017+lewishollis@users.noreply.github.com>
2024-06-13 21:26:50 +0100

    profilepicture logic

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,4 +2,14 @@ class FriendsController < ApplicationController
   def index
     @friends = Friend.all
   end
+
+  def profile_picture_url(friend_id)
+    friend = Friend.find_by(id: friend_id)
+    if friend
+      friend.profile_picture_url
+    else
+      # Handle case where friend with given ID doesn't exist
+      nil
+    end
+
 end
